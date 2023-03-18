181b6d1f17aba67335259d5cc935c00847ee892c
SpasPandev <sppan@abv.bg>
2023-03-18 09:35:26 +0200

    Empty project created

---
+++ src/main/java/com/example/practice_mobilelele/PracticeMobiLeLeLeApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_mobilelele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMobiLeLeLeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeMobiLeLeLeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practice_mobilelele/PracticeMobiLeLeLeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_mobilelele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMobiLeLeLeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
